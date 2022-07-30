0e1f70e4f13f216152430ce8ace928d25ea7f422
Tegar Sabila <tegarsabila11@gmail.com>
2022-07-31 05:37:54 +0700

    task weekly 1

---
+++ README.md
@@ -0,0 +1,8 @@
+# tugas bulan 1 weekly 1
+
+- Clone repo ini ke termux kalian
+- Lengkapi semua kode yang ada pada file main.py
+- Jika sudah buat branch baru dengan nama kalian
+- Lalu push tugas nya ke branch kalian
+
+Note: _tidak boleh push ke main_
---
+++ main.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python3
+
+#INFO : Lengkapi lah kode di bawah
+
+#TASK : Tugas 1
+#TODO : Task Isi variabel name dengan nama kamu
+
+#FIXME :
+name =
+
+#TODO : Lalu cetak lah tulisan 'Halo nama aku (nama_kamu) menggunakan format string'
+#NOTE : Untuk nama kamu tidak perlu pakai tanda kurung
+
+
+#TASK : Tugas 2
+#TODO : buatlah program menghitung luas bangun datar dengan syarat.
+# 1. Buat 2 variabel, 1 variabel dengan nama alas dengan tipe data float yang kedua buat variabel dengan nama tinggi dengan tipe data float
+# 2. lalu berikan nilai bebas pada masing-masing variabel
+# 3. Lalu buat variabel dengan nama luas untuk menghitung luas bangun datar
+# 4. lalu cetak hasil perhitungannya
+#NOTE : Jika tidak tahu rumus luas bangun datar bisa googling aja ya.
+
+
+
+# <Happy Weekend/>
