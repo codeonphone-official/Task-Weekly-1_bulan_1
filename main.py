b016c00d851249cd89f17ea4e1b156e713d57c6e
RainR6531 <rainr6531@gmail.com>
2022-08-01 16:42:39 +0800

    latihan1

--- main.py
+++ main.py
@@ -6,7 +6,8 @@
 #TODO : Task Isi variabel name dengan nama kamu
 
 #FIXME :
-name =
+name = "Rain"
+print("Halo nama aku", name)
 
 #TODO : Lalu cetak lah tulisan 'Halo nama aku (nama_kamu) menggunakan format string'
 #NOTE : Untuk nama kamu tidak perlu pakai tanda kurung
@@ -20,6 +21,22 @@ name =
 # 4. lalu cetak hasil perhitungannya
 #NOTE : Jika tidak tahu rumus luas bangun datar bisa googling aja ya.
 
+#FIXME :
+print("PERHITUNGAN LUAS BANGUN DATAR")
+print('''diketahui
+        alas    = 2.5
+        tinggi  = 1.5
+        ditanya = luas ?''')
+print("===LUAS JAJAR GENJANG===")
+alas    = 2.5
+tinggi  = 1.5
+luas    = alas * tinggi
+print("hasil luas bangun datar jajar genjang =", luas, type(luas))
+print("===LUAS SEGITIGA===")
+a = 2.5
+t = 1.5
+l = (a * t) / 2
+print("hasil luas bangun datar segitiga =", l, type(l))
 
 
 # <Happy Weekend/>
